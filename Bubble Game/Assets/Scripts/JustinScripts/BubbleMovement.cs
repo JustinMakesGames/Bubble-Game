860404bfc56fec9ed6e955eb9e2d8b3b47cd6285
JustinMakesGames <justinlf75@gmail.com>
2025-01-27 14:06:07 +0100

    Bubble Game movement added

---
+++ Bubble Game/Assets/Scripts/JustinScripts/BubbleMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleMovement : MonoBehaviour
+{
+    public float speed;
+    private float _horizontalMovement;
+    private float _verticalMovement;
+
+
+    private Rigidbody _rb;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        InputCheck();
+    }
+
+    private void InputCheck()
+    {
+        _horizontalMovement = Input.GetAxisRaw("Horizontal");
+        _verticalMovement = Input.GetAxisRaw("Vertical");
+
+    }
+    private void FixedUpdate()
+    {
+        Vector3 direction = new Vector3(_horizontalMovement, 0, _verticalMovement);
+
+        _rb.velocity = direction * speed;
+
+    }
+
+}
